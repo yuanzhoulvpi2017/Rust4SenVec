cd5c02860b07a744a61756a1ce5e0ddec82798fe
yuanz <yuanzhoulvpi@outlook.com>
2022-12-22 15:26:35 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ struct ModelInput {
     sentence: String,
 }
 
-#[derive(Serialize)]
+#[derive(Serialize, Debug)]
 struct ModelOutput {
     sentence: String,
     model_name: String,
@@ -38,64 +38,84 @@ impl Responder for ModelOutput {
     }
 }
 
-#[post("/nlpinfer")]
-async fn index(
-    model_input: web::Json<ModelInput>,
-    // model: web::Data<CModule>,
-    // tokenizer: web::Data<Tokenizer>,
-    appdata: web::Data<AppState>,
-) -> impl Responder {
-    let model = appdata.model.clone();
-    let tokenizer = appdata.tokenizer.clone();
-
-
-    let user_sentence: &String = &model_input.sentence;
-    let sentence_str = user_sentence.clone();
-
-    let encoding = tokenizer.encode(sentence_str, false).unwrap();
-
-    let encoding_ids_temp = encoding
-        .get_ids()
-        .iter()
-        .map(|&i| i as i32)
-        .collect::<Vec<i32>>();
-    let attention_mask_temp = encoding
-        .get_attention_mask()
-        .iter()
-        .map(|&i| i as i32)
-        .collect::<Vec<i32>>();
-
-    let attention_mask = Tensor::of_slice2(&[attention_mask_temp]);
-    let input_ids = Tensor::of_slice2(&[encoding_ids_temp]);
-
-    let result = model.forward_ts(&[input_ids, attention_mask]).unwrap();
-    let result_vector = Vec::<f32>::from(result);
-
-    let model_name = "test_bert".to_string();
-    ModelOutput {
-        sentence: user_sentence.clone(),
-        model_name: model_name.clone(),
-        vector: result_vector,
-    }
-}
-
 #[derive(Clone)]
-struct AppState {
+struct InferModel {
+    model_name: String,
     model: Arc<CModule>,
     tokenizer: Arc<Tokenizer>,
 }
 
+impl InferModel {
+    fn new(tokenizer_name: String, model_name: String) -> Self {
+        let tokenizer = Arc::new(Tokenizer::from_pretrained(tokenizer_name.clone(), None).unwrap());
+        let model = Arc::new(jit::CModule::load(model_name.clone()).unwrap());
+
+        Self {
+            model_name: tokenizer_name.clone(),
+            model: model,
+            tokenizer: tokenizer,
+        }
+    }
+
+    fn infer(&self, user_sentence: &String) -> ModelOutput {
+        let encoding = self.tokenizer.encode(user_sentence.clone(), false).unwrap();
+
+        let encoding_ids_temp = encoding
+            .get_ids()
+            .iter()
+            .map(|&i| i as i32)
+            .collect::<Vec<i32>>();
+        let attention_mask_temp = encoding
+            .get_attention_mask()
+            .iter()
+            .map(|&i| i as i32)
+            .collect::<Vec<i32>>();
+
+        let attention_mask = Tensor::of_slice2(&[attention_mask_temp]);
+        let input_ids = Tensor::of_slice2(&[encoding_ids_temp]);
+
+        let result = self.model.forward_ts(&[input_ids, attention_mask]).unwrap();
+        let result_vector = Vec::<f32>::from(result);
+
+        ModelOutput {
+            sentence: user_sentence.clone(),
+            model_name: self.model_name.clone(),
+            vector: result_vector,
+        }
+    }
+}
+
+// fn main() {
+//     let appdata = InferModel::new(
+//         "bert-base-cased".to_string(),
+//         "/Users/huzheng/PycharmProjects/Rust4SenVec/nlp_model/traced_bert.pt".to_string(),
+//     );
+
+//     let res = appdata.infer(&"hello world".to_string());
+    
+//     println!("{:?}", res);
+
+// }
+
+
+
+#[post("/nlpinfer")]
+async fn index(
+    model_input: web::Json<ModelInput>,
+    appdata: web::Data<InferModel>,
+) -> impl Responder {
+    let appdata = appdata.clone();
+    let result = appdata.infer(&model_input.sentence);
+    result
+
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    let appdata = AppState {
-        model: Arc::new(
-            jit::CModule::load(
-                "/Users/huzheng/PycharmProjects/Rust4SenVec/nlp_model/traced_bert.pt",
-            )
-            .unwrap(),
-        ),
-        tokenizer: Arc::new(Tokenizer::from_pretrained("bert-base-cased", None).unwrap()),
-    };
+    let appdata = InferModel::new(
+        "bert-base-cased".to_string(),
+        "/Users/huzheng/PycharmProjects/Rust4SenVec/nlp_model/traced_bert.pt".to_string(),
+    );
 
     HttpServer::new(move || {
         App::new()
